b27ba170b8cca034932380db62a1f4e3d2b25dc5
phj <iong274@sookmyung.ac.kr>
2021-08-27 02:38:28 +0900

    리사이클러뷰 테스트
    리사이클러뷰 테스트

---
+++ app/src/main/java/com/example/phpjsontest/cafe.java
@@ -0,0 +1,26 @@
+package com.example.phpjsontest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.io.Serializable;
+
+public class cafe implements Serializable  {
+
+
+        public String name;
+        public cafe() { }
+
+        public String getName()  {
+                return name;
+            }
+        public void setName(String name) {
+        this.name = name;
+    }
+        }
+
+
+
+
+
