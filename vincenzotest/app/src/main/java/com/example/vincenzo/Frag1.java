61c6941ba4afa11418b53e5dfcf820fc441fb66a
phj <iong274@sookmyung.ac.kr>
2021-08-30 02:34:05 +0900

    합본
    이미지 추가+사용자와 거리 계산해서 정렬+리사이클러뷰 잘림 현상 개선+상단바 삭제+두번째 페이지로 이름 가지고 넘어가서 전달

---
+++ vincenzotest/app/src/main/java/com/example/vincenzo/Frag1.java
@@ -0,0 +1,25 @@
+package com.example.vincenzo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Frag1 extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.frag1,container,false);
+
+        return view;
+    }
+
+}
